ab6f1df6e5ed973698bd126b1ec9c5cb4cd7f226
Kei Sugano <tobasojyo@gmail.com>
2024-08-29 01:13:17 +0900

    unwanted script removed

--- scripts/norm.sh
+++
@@ -1,6 +0,0 @@
-set -e -x
-name=$1
-mv ~/Desktop/$1.htm templates/old/norm/$1.htm
-rm -rf ~/Desktop/$1*
-
-vim templates/old/norm/$1.htm
--- scripts/ruby.sh
+++
@@ -1,5 +0,0 @@
-name=$1
-mv ~/Desktop/$1_files/$1.html templates/old/ruby/$1.htm
-
-rm -rf ~/Desktop/$1*
-vim templates/old/ruby/$1.htm
