b4198fad82e84667971a333adf336bfd4c140b29
Kei Sugano <tobasojyo@gmail.com>
2024-09-05 07:38:40 +0900

    twitter img impl

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,17 +21,25 @@ function Header({ bible }) {
         <link rel="icon" href="/static/favicon/favicon.ico" />
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>若者のためのキリスト教 - ストレスと精神的な悩みを乗り越える</title>
+        <title>ASMRキリスト教会</title>
         <meta name="description" content="10代〜20代の若者へ。ストレスや精神的な悩み、自殺願望に苦しむあなたに、キリスト教の教えと救いを提供します。睡眠障害や生きづらさを感じるあなたへ、聖書の言葉を通じて安らぎと希望を見つけましょう。まずうちさぁ、屋上あるんだけどさぁ、焼いてかない？（丁寧）" />
         <meta name="keywords" content="ストレス, 精神的な悩み, 自殺願望, 死にたい, 生きていたくない, キリスト教, 聖書, 安らぎ, 希望, 睡眠障害, 10代, 20代, 若者, 心のケア, メンタルヘルス, 毒親, パワハラ, メンヘラ, 親子関係, 人間関係, 聖書研究, プロテスタント, カルバン派, 野獣先輩" />
+
+        {/* Open Graph Meta Tags for Social Media Sharing */}
         <meta property="og:title" content="若者のためのキリスト教 - ストレスと精神的な悩みを乗り越える。やりますねえ！" />
         <meta property="og:description" content="10代〜20代の若者へ。キリスト教を通じて、ストレスや精神的な苦しみから救いを見つけましょう。死にたい、生きていたくないと感じているあなたへ、聖書の言葉が希望をもたらします。田所浩二のことはいったん忘れてください！" />
+        <meta property="og:image" content={`${process.env.PUBLIC_URL}/static/images/thumbnail.png`} /> {/* Replace with actual image URL */}
+        <meta property="og:url" content="https://asmrchurch.com" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="若者のためのキリスト教 - ストレスと精神的な悩みを乗り越える。みたきゃみせてやるよ。おれの信仰" />
         <meta name="twitter:description" content="10代〜20代の若者へ。ストレスや精神的な悩み、自殺願望に苦しむあなたに、キリスト教の教えと救いを提供します。睡眠障害や生きづらさを感じるあなたへ、聖書の言葉を通じて安らぎと希望を見つけましょうと語っていると不幸にも黒塗りの高級車に衝突してしまう。示談の条件とは・・・あくしろよ（エコー）" />
+        <meta name="twitter:image" content={`${process.env.PUBLIC_URL}/static/images/thumbnail.png`} /> {/* Replace with actual image URL */}
+
+        {/* Robots and author information */}
         <meta name="robots" content="index, follow" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://yourwebsite.com" />
         <meta name="author" content="ASMRキリスト教会" />
       </Helmet>
 
