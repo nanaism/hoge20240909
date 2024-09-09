c309e847c9988c86343794c5cc539dbbf84795c8
nanaism <datealife2525@gmail.com>
2024-09-09 23:21:34 +0900

    fix: 省略されていた箇所を修正

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import "../styles.css";
+import Ham from "./Ham";
 
 function Header({ bible, title, description, url, image }) {
   const [pageType, setState] = useState("norm");
@@ -93,7 +94,76 @@ function Header({ bible, title, description, url, image }) {
         />
       </Helmet>
 
-      {/* Rest of the component remains the same */}
+      <div className="c1">
+        <a href="/">聖書ASMR</a>
+        {bible && (
+          <>
+            <span className="sep"></span>
+            <button className="toggle-button" onClick={toggleRuby}>
+              {pageType === "norm" ? "聖" : pageType === "ruby" ? "せ" : "E"}
+            </button>
+          </>
+        )}
+        {!bible && (
+          <>
+            <span className="sep"></span>
+            <button className="toggle-button" onClick={toggleLang}>
+              {pageType === "norm" ? "聖" : "E"}
+            </button>
+          </>
+        )}
+      </div>
+
+      <div className="c3">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://x.com/asmrchurch"
+        >
+          <img
+            src={`${process.env.PUBLIC_URL}/static/images/x.png`}
+            alt="X (formerly Twitter) logo"
+            style={{ width: "35px", height: "35px" }}
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.youtube.com/@asmrchurch"
+        >
+          <img
+            src={`${process.env.PUBLIC_URL}/static/images/youtube.png`}
+            alt="YouTube logo"
+            style={{ width: "35px", height: "35px" }}
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://asmrchruch.bandcamp.com/"
+        >
+          <img
+            src={`${process.env.PUBLIC_URL}/static/images/bandcamp.png`}
+            alt="Bandcamp logo"
+            style={{ width: "35px", height: "35px" }}
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.tiktok.com/@sexybible"
+        >
+          <img
+            src={`${process.env.PUBLIC_URL}/static/images/tiktok.png`}
+            alt="TikTok logo"
+            style={{ width: "35px", height: "35px" }}
+          />
+        </a>
+      </div>
+
+      <div className="ham">
+        <Ham />
+      </div>
     </div>
   );
 }
